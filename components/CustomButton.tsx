9890c2612342d935bf9e46dfcf7eacead7e8bdbf
Luis Gabriel <luis.andrade@markts.com.br>
2024-07-04 10:02:35 -0300

    feat: add custom button

---
+++ components/CustomButton.tsx
@@ -0,0 +1,33 @@
+import { Text, TouchableOpacity } from "react-native";
+
+interface CustomButtonProps {
+  title: string;
+  handlePress: any;
+  containerStyles?: string;
+  textStyles?: string;
+  isLoading: boolean;
+}
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      disabled={isLoading}
+      className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
