d38868cb01a816f29444e0d574f3492fc94f7c55
Luis Gabriel <luis.andrade@markts.com.br>
2024-07-05 17:20:59 -0300

    feat: estilizando tela de autenticação

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,16 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>Auth</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,7 +1,46 @@
-import { Text } from "react-native";
+import FormField from "@/components/FormField";
+import { images } from "@/constants";
+import { useState } from "react";
+import { Text, View, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
-    return ( <Text>oii</Text> );
-}
- 
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full flex justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e: string) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e: string) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
 export default SignIn;
